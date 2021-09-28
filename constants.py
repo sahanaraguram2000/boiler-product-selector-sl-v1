3bfa74eced0b7d79b9fe5e676ebd449a8c71533f
Sahana Raguram <68541655+sahanaraguram2000@users.noreply.github.com>
2021-09-28 08:52:44 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,137 +7,60 @@ opco_dict = {
     'Canada' : [0,0,0,0,1,0],
     'EMEA' : [0,0,0,0,0,1]
 }
-
-psig1_dict = {
-    'Yes':1,
-    'No':0
+op_pres_dict={
+    'Less than 60 Bar': 1,
+    'Between 10 to 60 Bar': 2
 }
 
-psig2_dict = {
-    'Yes':1,
-    'No':0
-}
-
-sa_dict = {
-    'Yes':1,
-    'No':0
-}
 
 fw_dict = {
-    'Raw water':1,
-    'Demineralized':3,
-    'RO':2
+    'All (Raw, RO, Demin)':1,
+    'Raw, RO only':2
 }
 
-internal_dict = {
+fda_dict ={
     'Yes':1,
     'No':0
 }
-
-multipurpose_dict = {
-    'Yes':1,
-    'No':0
-}
-
-sl_form_dict = {
-    'Solid':1,
-    'Liquid':2
-}
-
-co_ord_dict = {
-    'Yes':1,
-    'No':0
-}
-
-po4_dict = {
-    'Yes':1,
-    'No':0
-}
-
-cpo4_dict = {
-    'Yes':1,
-    'No':0
-}
-
-all_poly_dict = {
-    'Yes':1,
-    'No':0
-}
-
-na_po4_dict = {
-    'Not applicable' : 1,
-    'Not recommended' : 2
-}
-
 dairy_dict = {
     'Yes':1,
     'No':0
 }
 
-food_dict = {
+func_type_dict = {
+    'Multi Functional (Internal + Amine + O2)':1,
+    'Multi Functional (Amine + O2)':3,
+    'Multi Functional (Internal + Defoamer)':2,
+    'Single Functional Products':4
+}
+sl_dict = {
+    'Solid':1,
+    'Liquid':2
+}
+internal_dict = {
     'Yes':1,
     'No':0
 }
-
-fda_dict ={
-    'Yes (refer to US FDA for details or contact SME)':1,
+po4_dict = {
+    'Yes':1,
     'No':0
 }
-
 defoamer_dict = {
     'Yes':1,
     'No':0
 }
-
-avt_dict = {
-    'Yes':1,
-    'No':0
-}
-
 n_amine_dict = {
     'Yes':1,
     'No':0
 }
-v_n_amine_dict = {
-    'Yes':1,
-    'No':0
-}
-steam_dict = {
-    'Yes':1,
-    'No':0
-}
-
-turbine_dict = {
-    'Compatible, not beneficial':1,
-    'Yes':2,
-    'Compatible, not beneficial, true but would never be used in a plant with a turbine':3
-}
-
 yellow_dict = {
     'No':0,
     'Yes':1
 }
-
 dr_dict = {
-    'Not applicable': 3,
-    '< 2.4': 1,
-    '> 2.4':2
-}
-
-f_amine_dict = {
-    'Yes':1,
-    'No':0
-}
-
-tot_amine_dict = {
-    '0.0':1,
-    '0.9':2,
-    '4.8':3,
-    '30.0':4,
-    '35.0':5,
-    '40.0':6,
-    '42.0':7,
-    '45.0':8
+    '< 2.4':1,
+    '> 2.4':2,
+    'Not Applicable':3
 }
 
 ox_scav_dict = {
